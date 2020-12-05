8fa3cede9bb5afe33cfd14885d45f4377cf4e45d
Сергей Гаврилов <ses3332002@gmail.com>
2020-12-05 10:47:19 +0200

    rewrited with ES6 classes

--- index.js
+++ index.js
@@ -8,80 +8,74 @@
 // ======== OBJECTS DEFINITIONS ========
 // Define your objects here
 
-const dog = {
-  species: "dog",
-  name: "Toby",
-  gender: "male",
-  legs: 4,
-  hands: 0,
-  saying: "Woof-woof!",
-  friends: [
-    "Puss"
-  ]
+class Being {
+  constructor (name, friends, species) {
+    this.species = species;
+    this.name = name;
+    this.friends = friends;
+  }
+  
+  getDescription() {
+    return Object.entries(this)
+    .filter(([key]) => propsToPrint.includes(key))
+    .map(([,value]) => Array.isArray(value) ? value.join(', ') : value)
+    .join('; ');
+  }
 };
 
-const cat = {
-  species: "cat",
-  name: "Puss",
-  gender: "male",
-  legs: 4,
-  hands: 0,
-  saying: "Meow!",
-  friends: [
-    "Toby",
-    "Halley"
-  ]
+class Animal extends Being {
+  constructor (name, friends, gender, saying, species) {
+    super(name, friends, species);
+    this.gender = gender;
+    this.saying = saying;
+    this.legs = 4;
+    this.hands = 0;
+  }
 };
 
-const man = {
-  species: "human",
-  name: "Jack",
-  gender: "male",
-  legs: 2,
-  hands: 2,
-  saying: "Hey baby!",
-  friends: [
-    "Jennie",
-    "Halley"
-  ]
+class Dog extends Animal {
+  constructor (name, friends, gender) {
+    super(name, friends, gender);
+    this.species = "dog";
+    this.saying = "Woof-woof!";
+  }
 };
 
-const woman = {
-  species: "human",
-  name: "Jennie",
-  gender: "female",
-  legs: 2,
-  hands: 2,
-  saying: "Hi darling!",
-  friends: [
-    "Jack",
-    "Toby"
-  ]
+class Cat extends Animal {
+  constructor (name, friends, gender) {
+    super(name, friends, gender);
+    this.species = "cat";
+    this.saying = "Meow!";
+  }
 };
 
-const catWoman = {
-  species: "super-hero",
-  name: "Halley",
-  gender: "female",
-  legs: 2,
-  hands: 2,
-  saying: cat.saying,
-  friends: [
-    "Puss"
-  ]
+class Human extends Animal {
+  constructor (name, friends, gender, saying) {
+    super(name, friends, gender, saying);
+    this.species = "human";
+    this.legs = 2;
+    this.hands = 2;
+  }
 };
+
+class Superhero extends Human {
+  constructor (name, friends, gender, saying) {
+    super(name, friends, gender);
+    this.species = "super-hero";
+  }
+};
+
+const dog = new Dog ("Toby", ["Puss"], "male");
+const cat = new Cat ("Puss", ["Toby", "Halley"], "male");
+const man = new Human ("Jack", ["Jennie", "Halley"], "male", "Hey baby!");
+const woman = new Human ("Jennie", ["Jack", "Toby"], "female", "Hi darling!");
+const catWoman = new Superhero ("Halley", ["Puss"], "female");
+catWoman.saying = new Cat().saying;
 
 const inhabitants = [dog, cat, man, woman, catWoman];
 const propsToPrint = ["species", "name", "gender", "legs", "hands", "friends", "saying"];
 
-inhabitants.forEach(item => {
-  print(
-    Object.entries(item)
-      .filter(([key]) => propsToPrint.includes(key))
-      .map(([,value]) => Array.isArray(value) ? value.join(', ') : value)
-      .join('; ')
-  );
-});
+inhabitants.forEach(item => print(item.getDescription()));
 
 // ======== OUTPUT ========
 /* Use print(message) for output.
